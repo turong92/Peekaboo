0615cd760e9fec4b2509c100a39fe7ef16c7444c
turong92 <rkclrhks33@gmail.com>
2021-05-28 23:30:13 +0900

    oauth2 로그인 8080 환경에서 data 넘어오는것까지 확인

---
+++ peekaboo/src/main/java/com/peekaboo/configuration/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.peekaboo.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.peekaboo.configuration.oauth.PrincipalOauth2UserService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/user/**").authenticated()
+			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			//.loginPage("/view-test")
+			.loginProcessingUrl("/login")
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.userInfoEndpoint()
+			.userService(principalOauth2UserService);
+	}
+}
